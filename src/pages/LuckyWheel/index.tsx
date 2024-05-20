d4a34e0b4e7a68b9ef3ebab345647b68ae60b685
miloliu-xyz <miloliu@ethsign.xyz>
2024-05-20 16:25:12 +0800

    feat: link page state

--- src/pages/LuckyWheel/index.tsx
+++ src/pages/LuckyWheel/index.tsx
@@ -1,13 +1,48 @@
+import { Card } from '@/components/Card';
 import { LuckyWheel } from '@/components/LuckyWheel';
-import { Result } from '@/components/Result.tsx';
-import React from 'react';
+import { CoinsStacked01 } from '@ethsign/icons';
+import React, { useEffect, useState } from 'react';
+import { LuckyWheelPageContext, LuckyWheelPageData, initPageData } from './context';
+
+let debugFlag = false;
 
 export const LuckyWheelPage: React.FC = () => {
-  return (
-    <>
-      <LuckyWheel />
+  const [pageData, setPageDate] = useState<LuckyWheelPageData>(initPageData);
 
-      <Result />
-    </>
+  const fetchPageData = async () => {
+    // TODO: fetch page data from server
+    setPageDate({
+      ...initPageData,
+      currentScore: debugFlag ? 1000 : 0,
+      hasSpinedToday: !debugFlag
+    });
+
+    debugFlag = true;
+  };
+
+  useEffect(() => {
+    fetchPageData;
+  }, [setPageDate]);
+
+  return (
+    <LuckyWheelPageContext.Provider
+      value={{
+        ...pageData,
+        refresh: fetchPageData
+      }}
+    >
+      <div className="space-y-5">
+        <Card>
+          <div className="text-center font-bold text-white">Sign Score</div>
+          <div className="mt-2 flex items-center justify-center gap-2 text-3xl">
+            <CoinsStacked01 size={24} color="#FCFCFD" />
+
+            <span> {pageData.totalScore}</span>
+          </div>
+        </Card>
+
+        <LuckyWheel />
+      </div>
+    </LuckyWheelPageContext.Provider>
   );
 };
