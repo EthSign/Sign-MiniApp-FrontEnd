14a8d00f97f69a376c6708d01f6274ec58cce08a
miloliu-xyz <miloliu@ethsign.xyz>
2024-05-24 14:32:27 +0800

    feat: extract raffle wheel component

--- src/pages/LuckyWheel/components/LuckyWheel.tsx
+++ src/pages/LuckyWheel/components/LuckyWheel.tsx
@@ -1,98 +1,11 @@
 import { useLotteryInfo } from '@/providers/LotteryInfoProvider';
-import { raffle } from '@/services';
 import classNames from 'classnames';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Transition } from 'react-transition-group';
 import { Card } from '../../../components/Card';
 import { Result } from '../../../components/Result';
+import { RaffleWheel } from '../../../components/RaffleWheel';
 import { Score } from './Score';
-import WebApp from '@twa-dev/sdk';
-
-export const Wheel = React.forwardRef<
-  HTMLDivElement,
-  { className?: string; onResult?: () => void; onStopped?: () => void }
->((props, ref) => {
-  const { loading, prizes, refresh } = useLotteryInfo();
-
-  const { className, onResult, onStopped } = props;
-
-  const [isSpining, setIsSpining] = useState(false);
-
-  const [isRaffling, setIsRaffling] = useState(false);
-
-  const [degree, setDegree] = useState(0);
-
-  const onSpinButtonClick = async () => {
-    if (loading || isSpining || isRaffling) return;
-
-    setIsRaffling(true);
-
-    const raffleResult = await raffle().catch((error) => {
-      refresh();
-      throw error;
-    });
-
-    setIsRaffling(false);
-
-    try {
-      WebApp.HapticFeedback.impactOccurred('heavy');
-    } catch (error) {
-      console.error(error);
-    }
-
-    const prizeIndex = prizes.findIndex((item) => item.id === raffleResult.prizeId);
-
-    onResult?.();
-
-    setDegree(3600 - prizeIndex * 60 || 0);
-
-    setIsSpining(true);
-  };
-
-  return (
-    <div
-      ref={ref}
-      className={classNames(
-        'relative flex aspect-square w-[420px] shrink-0 items-center justify-center overflow-hidden select-none',
-        className
-      )}
-    >
-      <div className="absolute inset-0">
-        <img src="/wheel-bg.svg" className="object-cover" alt="" />
-      </div>
-
-      <div className="absolute inset-[40px]">
-        <img
-          src="/wheel.svg"
-          style={{
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
-            '--tw-rotate': degree + 'deg'
-          }}
-          className={classNames('rotate-0 object-contain [transition-timing-function:cubic-bezier(0.5,0,0,1)]', {
-            '[transition-duration:5s] transition-all': isSpining
-          })}
-          onTransitionEnd={() => {
-            onStopped?.();
-            setIsSpining(false);
-            setDegree(degree - 3600);
-          }}
-        />
-      </div>
-
-      <img src="/wheel-pointer.svg" className="absolute left-1/2 top-0 w-[18px] -translate-x-1/2" alt="" />
-
-      <div className="z-10 flex size-[49px] items-center justify-center overflow-hidden rounded-full bg-[linear-gradient(225deg,#FEDC31_4.15%,#FDC347_13.8%,#FC8682_33.1%,#FA2CD7_59.91%,#987CDB_85.64%,#33D0E0_111.37%)]">
-        <div
-          className="relative z-10 flex size-[44px] items-center justify-center rounded-full bg-black font-bold text-[16px]"
-          onClick={onSpinButtonClick}
-        >
-          Spin
-        </div>
-      </div>
-    </div>
-  );
-});
 
 export const LuckyWheel: React.FC = () => {
   const { hasSpinedToday, currentScore, refresh } = useLotteryInfo();
@@ -133,7 +46,7 @@ export const LuckyWheel: React.FC = () => {
 
         <Transition nodeRef={wheelRef} in={!hasSpinedToday} unmountOnExit timeout={200}>
           {(state) => (
-            <Wheel
+            <RaffleWheel
               ref={wheelRef}
               className={classNames('scale-100 transition-all duration-200 origin-center', {
                 '!scale-50 opacity-0': state === 'exiting'
