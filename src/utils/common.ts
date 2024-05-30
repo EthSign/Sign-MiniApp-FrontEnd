8835de57eb3502298e62d828484a531ea1a8825b
YanYuan <yan950414@gmail.com>
2024-05-30 16:53:13 +0800

    fix: remove debug data

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -27,11 +27,11 @@ interface ITMAInitData {
   start_param?: string; //code
 }
 
-const debugData =
-  'user=%7B%22id%22%3A1312579605%2C%22first_name%22%3A%22Evan%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22yanyuanfe%22%2C%22language_code%22%3A%22zh-hans%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-6858595359197215319&chat_type=supergroup&start_param=eyJyYWZmbGVJZCI6InFFOUJvMGJ3NkppLVFKR1czaEh0XyIsImludml0ZVVzZXIiOiJ5YW55dWFuZmUifQ%3D%3D&auth_date=1717034965&hash=16b53392ba124947885b8af0de9e85559c22618e1a80d20c2cbed6357086532c';
+// const debugData =
+//   'user=%7B%22id%22%3A1312579605%2C%22first_name%22%3A%22Evan%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22yanyuanfe%22%2C%22language_code%22%3A%22zh-hans%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-6858595359197215319&chat_type=supergroup&start_param=eyJyYWZmbGVJZCI6InFFOUJvMGJ3NkppLVFKR1czaEh0XyIsImludml0ZVVzZXIiOiJ5YW55dWFuZmUifQ%3D%3D&auth_date=1717034965&hash=16b53392ba124947885b8af0de9e85559c22618e1a80d20c2cbed6357086532c';
 
 export const getTMAInitData = (): ITMAInitData | null => {
-  const initDataRaw = window.Telegram.WebApp?.initData || debugData; // user=...&query_id=...&...
+  const initDataRaw = window.Telegram.WebApp?.initData; // user=...&query_id=...&...
   console.log(window.Telegram.WebApp?.initData, 'initDataRaw');
 
   if (!initDataRaw) return null;
